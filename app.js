7b3c037208ba1f0792d2e8349ffd55f965d5f453
“Marcela <marcelavieirafreire@gmail.com>
2020-04-13 15:06:54 -0300

    Project structure done

---
+++ app.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const app_name = require('./package.json').name;
+
+//banco de dados
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true})
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+  const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app = express();
+
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// const auth = require('./routes/auth');
+const index = require('./routes/index');
+app.use('/', index);
+// app.use('/', auth);
+
+
+
+module.exports = app;
