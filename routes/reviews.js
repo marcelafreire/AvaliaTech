08adc5c2617ae87d5927647399e494b50944ff46
Felipe Massao <massao.felipe@gmail.com>
2020-04-20 11:33:42 -0300

    hotfix to allow axios calls in deploy application

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,27 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const ensureLogin = require("connect-ensure-login");
+const ensureLogin = require('connect-ensure-login');
 const Course = require('../models/course');
 const User = require('../models/user');
 const Review = require('../models/review');
-const session = require("express-session");
-
+const session = require('express-session');
 
 //Review API
-router.put('/api/review/:id', (req, res) => {
-    const userId = req.user._id
+router.put('/course/:courseID/api/review/:id', (req, res) => {
+	const userId = req.user._id;
 	const { text, rating } = req.body;
 	const { id } = req.params;
 	console.log(id);
-	Review.findOneAndUpdate({ _id: id }, { text, rating, writer:userId }, { new: true })
+	Review.findOneAndUpdate({ _id: id }, { text, rating, writer: userId }, { new: true })
 		.then((review) => {
-              res.json(review)
-
+			res.json(review);
 		})
 		.catch((err) => console.log(err));
 });
 
-router.delete('/api/review/:id', (req, res) => {
+router.delete('/course/:courseID/api/review/:id', (req, res) => {
 	const { id } = req.params;
 
 	Review.findOneAndDelete({ _id: id })
@@ -32,27 +30,32 @@ router.delete('/api/review/:id', (req, res) => {
 		.catch((err) => console.log(err));
 });
 
-
 router.post('/reviews/add/:id', ensureLogin.ensureLoggedIn(), (req, res) => {
-	const userId = req.user._id
-	const {text, rating} = req.body;
-    const {id} = req.params
+	const userId = req.user._id;
+	const { text, rating } = req.body;
+	const { id } = req.params;
 
-	User.findOne({_id: userId})
-	.then(user => {	
-        console.log(user)
+	User.findOne({ _id: userId })
+		.then((user) => {
+			console.log(user);
 
-	Review.create({text, rating, writer:user})
-	.then(review => {
-		Course.findOneAndUpdate({_id: id}, {$push: { reviews: review}})
-		.then(course => {
-			res.redirect(`/course/${course._id}`)
+			Review.create({ text, rating, writer: user })
+				.then((review) => {
+					Course.findOneAndUpdate({ _id: id }, { $push: { reviews: review } })
+						.then((course) => {
+							res.redirect(`/course/${course._id}`);
+						})
+						.catch((error) => {
+							console.log(error);
+						});
+				})
+				.catch((error) => {
+					console.log(error);
+				});
 		})
-		.catch((error) => {console.log(error)})
-	})
-	.catch((error) => {console.log(error)})
-  })
-  .catch((error) => {console.log(error)})
+		.catch((error) => {
+			console.log(error);
+		});
 });
 
 module.exports = router;
