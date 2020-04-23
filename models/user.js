2ac634a43574ec564fefae6115ed33959cb7e147
Felipe Massao <massao.felipe@gmail.com>
2020-04-23 17:47:48 -0300

    change default image profile

--- models/user.js
+++ models/user.js
@@ -1,31 +1,30 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  role: {
-    type: String,
-    enum : ['GUEST', 'EDITOR', 'ADMIN'],
-    default : 'EDITOR'
-  },
-  email: String,
-  imgName: String,
-  imgPath: {
-    type: String,
-    default : 'https://res.cloudinary.com/deyy3glzl/image/upload/v1587146626/download_ugpihi.png'
-  }
-}, 
+const userSchema = new Schema(
+	{
+		username: {
+			type: String
+		},
+		password: {
+			type: String
+		},
+		role: {
+			type: String,
+			enum: [ 'GUEST', 'EDITOR', 'ADMIN' ],
+			default: 'EDITOR'
+		},
+		email: String,
+		imgName: String,
+		imgPath: {
+			type: String,
+			default: 'https://res.cloudinary.com/deyy3glzl/image/upload/v1587652134/perfil-avalia_uekcwz.png'
+		}
+	},
+	{
+		timestamps: true
+	}
+);
 
-{
-  timestamps: true
-});
-
-
-
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 module.exports = User;
